aeac1c4e36a14bd5126d45f47aa32c57cf77d0be
Devon Smith <devo8604@gmail.com>
2013-06-05 18:59:00 -0800

    Initial Commit

--- iOS UI Test/ViewController.m
+++ iOS UI Test/ViewController.m
@@ -6,24 +6,41 @@
 //  Copyright (c) 2013 Devon Smith. All rights reserved.
 //
 
+/*
+    Alerts send an alert to the user and lets them know about something that is going to affect 
+ the app. It mostly good for warnings or to let the user know of an unexpected error that has 
+ popped up. Action sheets are used to alert the user that they are about to do something that
+ could be dangerous and lets the user pick an option on how to handle that particular situation.
+ An example of this could be resetting your iPhone to it's default settings.
+ */
+
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
+
+@synthesize textValue;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)btnClicked:(id)sender {
+    NSString *str = [[NSString alloc]
+                     initWithFormat:@"Hello %@!", textValue.text];
+    
+    UIAlertView *alert = [[UIAlertView alloc]
+                          initWithTitle: @"Howdy!!"
+                          message:str
+                          delegate: self
+                          cancelButtonTitle:@"Go Away"
+                          otherButtonTitles:nil];
+    [alert show];
 }
 
 @end
